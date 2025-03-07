8fadde950212e49db52b6d26612d71e16cf3264c
Phạm Minh Hiếu <hieupham110303@gmail.com>
2025-03-07 13:54:51 +0700

    l

---
+++ src/main/java/com/Book/System/Repo/BookRepository.java
@@ -0,0 +1,10 @@
+package com.Book.System.Repo;
+
+import com.Book.System.Model.Book;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+    List<Book> findByTitle(String title);
+}
--- src/main/java/com/Book/System/SystemApplication.java
+++ src/main/java/com/Book/System/SystemApplication.java
@@ -2,12 +2,15 @@ package com.Book.System;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.Book.System.Repo")
+@EntityScan("com.Book.System.Model")
 @SpringBootApplication
 public class SystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SystemApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SystemApplication.class, args);
+    }
 }
